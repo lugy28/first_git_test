13e1cb1fe6c67802f68dbe44c9510c9f86d44d2c
lgy <lu18935332507@163.com>
2025-11-28 22:14:51 +0800

    更新c#代码文件

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s ;
+            for (int i = 0; i<5; i++)
+            {
+                s = 100;
+                Console.WriteLine(s);
+            }
+            
+           // Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
